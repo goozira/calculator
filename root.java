c74429d18eda06e1e88e4c736d7628989ca138ca
Marcellus Redd <goozira@gmail.com>
2016-08-26 04:36:12 -0500

    Add class files and create Documentation folder

--- root.java
+++ root.java
@@ -4,5 +4,6 @@ public class root
   double root(double firstTerm, double secondTerm)
   {
     // hard to implement right now.  Need some research.
+    return 0;
   }
 }
--- subtract.java
+++ subtract.java
@@ -1,7 +1,7 @@
 
 public class subtract
 {
-  int subtract(int firstTerm, secondTerm)
+  int subtract(int firstTerm, int secondTerm)
   {
     return firstTerm - secondTerm;
   }
